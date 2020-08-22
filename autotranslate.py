c0b184d511bc1d4874fdb4aabf442270a7640a0d
fah-OwO <65396522+fah-OwO@users.noreply.github.com>
2020-08-22 15:52:25 +0700

    Update autotranslate.py

--- autotranslate.py
+++ autotranslate.py
@@ -3,34 +3,44 @@ import tkinter as tk
 import win32api
 import googletrans as gt
 import win32clipboard
-import win32api,win32con
 import codecs
+
 translator = gt.Translator()
-a=translator.translate('สวัสดี').text
-print(a)
-lastdata=''
-lastdatatranslate=''
+def getclipboard():
+    try:
+        win32clipboard.OpenClipboard()
+        data = win32clipboard.GetClipboardData()
+        win32clipboard.CloseClipboard()
+    except Exception as e:
+        data=str(e)
+    return data
+def translt(data):
+    en=translator.translate(data,dest="en")
+    th=translator.translate(data,dest="th")
+    d=en.text+'\n'+th.text
+    if en.src=='ja':
+        ja=translator.translate(data,dest="ja")
+        d=ja.pronunciation+'\n'+d
+    return d
+
+lastdata=getclipboard()
+lastdatatranslate=translt(lastdata)
 def tkloop():
     global lastdata,lastdatatranslate
-    if not win32api.GetKeyState(0x43)<0:
-        try:
-            win32clipboard.OpenClipboard()
-            data = win32clipboard.GetClipboardData()
-            win32clipboard.CloseClipboard()
-        except:
-            print('error')
-            data=' '
-            time.sleep(1)
+    # global state_Ckey,button_Ckey
+    # while True:
+    #     now_Ckey=win32api.GetKeyState(button_Ckey)
+    #     if now_Ckey<0 and now_Ckey!=state_Ckey :
+    #         state_Ckey=now_Ckey
+    #         break
+    #     time.sleep(0.01)
+    
+    if  win32api.GetKeyState(0x43)<0:
+        data=getclipboard()
         if data != lastdata:
-            en=translator.translate(data,dest="en")
-            th=translator.translate(data,dest="th")
-            
-            d=en.text+'\n'+th.text
-            if en.src=='ja':
-                ja=translator.translate(data,dest="ja")
-                d=ja.pronunciation+'\n'+d
+            d=translt(data)
             lastdatatranslate=d
-            print(u"data =\t{}\nenglish =\t{}\nthai =\t{}".format(data,en,th))
+            # print(u"data =\t{}\nenglish =\t{}\nthai =\t{}".format(data,en,th))
             lastdata=data
         else:
             d=lastdatatranslate
@@ -42,9 +52,18 @@ button_c=0x43
 state_c=win32api.GetKeyState(button_c)
 
 root = tk.Tk()
+root.title("auto translate")
 root.attributes("-topmost", True)
 root.bind('<Escape>', lambda e: root.quit())
-text = tk.Text(root,height=10,width=30,font="Sans 20",padx=50,pady=10)
+text = tk.Text(root,height=20,width=20,font="Sans 20",padx=50,pady=10)
+text.insert(1.0,lastdatatranslate)
 text.pack()
 tkloop()
 root.mainloop()
+# while now_Ckey<0 and now_Ckey!=state_Ckey :
+#     now_Ckey=win32api.GetKeyState(button_Ckey)
+#     print(state_Ckey,now_Ckey)
+#     if now_Ckey<0 and now_Ckey!=state_Ckey :
+#         state_Ckey=now_Ckey
+#         print('aaaaaa')
+#     time.sleep(0.01)
