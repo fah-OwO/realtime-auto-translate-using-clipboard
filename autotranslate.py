eea3b6261f1250406825fbd15d316fab957ed7b7
fah-OwO <65396522+fah-OwO@users.noreply.github.com>
2020-09-20 15:10:53 +0700

    Update autotranslate.py
    Tk have get clipboard too

--- autotranslate.py
+++ autotranslate.py
@@ -2,18 +2,9 @@ import time
 import tkinter as tk
 import win32api
 import googletrans as gt
-import win32clipboard
 import codecs
 
 translator = gt.Translator()
-def getclipboard():
-    try:
-        win32clipboard.OpenClipboard()
-        data = win32clipboard.GetClipboardData()
-        win32clipboard.CloseClipboard()
-    except Exception as e:
-        data=str(e)
-    return data
 def translt(data):
     en=translator.translate(data,dest="en")
     th=translator.translate(data,dest="th")
@@ -23,12 +14,19 @@ def translt(data):
         d=ja.pronunciation+'\n'+d
     return d
 
-lastdata=getclipboard()
+root = tk.Tk()
+root.title("auto translate")
+root.attributes("-topmost", True)
+root.bind('<Escape>', lambda e: root.quit())
+text = tk.Text(root,height=20,width=20,font="Sans 20",padx=50,pady=10)
+text.insert(1.0,lastdatatranslate)
+text.pack()
+lastdata=root.clipboard_get()
 lastdatatranslate=translt(lastdata)
 def tkloop():
     global lastdata,lastdatatranslate
     if  win32api.GetKeyState(0x43)<0:
-        data=getclipboard()
+        data=root.clipboard_get()
         if data != lastdata:
             d=translt(data)
             lastdatatranslate=d
@@ -38,14 +36,6 @@ def tkloop():
         text.delete(1.0,tk.END)
         text.insert(1.0,d)
     text.after(50, tkloop)
-
-
-root = tk.Tk()
-root.title("auto translate")
-root.attributes("-topmost", True)
-root.bind('<Escape>', lambda e: root.quit())
-text = tk.Text(root,height=20,width=20,font="Sans 20",padx=50,pady=10)
-text.insert(1.0,lastdatatranslate)
-text.pack()
 tkloop()
+
 root.mainloop()
