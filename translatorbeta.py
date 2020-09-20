3ca48475c6278bf42126f4dc61cbd13ad303d047
fah-OwO <65396522+fah-OwO@users.noreply.github.com>
2020-09-20 15:13:44 +0700

    Update translatorbeta.py

--- translatorbeta.py
+++ translatorbeta.py
@@ -11,28 +11,12 @@ a=translator.translate('สวัสดี').text
 print(a)
 lastdata=''
 lastdatatranslate=''
-# def translate():
-#     win32clipboard.OpenClipboard()
-#     data = win32clipboard.GetClipboardData()
-#     win32clipboard.CloseClipboard()
-#     # return translator.translate(data,src='ja').text
-#     return translator.translate(data,dest="en").text+'\n'+translator.translate(data,dest="th").text
-
 button_Ckey=0x43
-state_Ckey=win32api.GetKeyState(button_Ckey)
 def tkloop():
-    # global button_c
-    # global state_c
-    # now_c=win32api.GetKeyState(button_c)
-    # if now_c<0 and now_c!=state_c :
-        # state_c=now_c
-        # time.sleep(0.005)
     global lastdata,lastdatatranslate
     try:
         time.sleep(0.2)
-        win32clipboard.OpenClipboard()
-        data = win32clipboard.GetClipboardData()
-        win32clipboard.CloseClipboard()
+        data=root.clipboard_get()
     except:
         print('error')
         data=' '
@@ -54,14 +38,9 @@ def tkloop():
     text.insert(1.0,d)
     text.after(5, tkloop)
 
-button_c=0x43
-state_c=win32api.GetKeyState(button_c)
-
 root = tk.Tk()
 root.attributes("-topmost", True)
 root.bind('<Escape>', lambda e: root.quit())
-# lmain = tk.Label(root)
-# lmain.pack()
 text = tk.Text(root,height=10,width=30,font="Sans 20",padx=50,pady=10)
 text.pack()
 tkloop()
